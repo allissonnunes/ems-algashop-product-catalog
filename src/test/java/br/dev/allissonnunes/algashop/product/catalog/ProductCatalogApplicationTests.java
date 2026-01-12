760e27e8f400459984abf582616e8611e66022da
Allisson Alves Batista Nunes <allissonabn@gmail.com>
2026-01-12 15:53:36 -0300

    Add product-catalog microservice with basic Spring Boot setup
    Introduces the product-catalog microservice with a Spring Boot structure, configurations, Gradle setup, and initial test file. Includes necessary submodule configuration and essential files for development and build management.

---
+++ src/test/java/br/dev/allissonnunes/algashop/product/catalog/ProductCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.allissonnunes.algashop.product.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
