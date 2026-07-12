f3c1cae230ac8b504aa3dddfcf924eaf1a22412b
Allisson Alves Batista Nunes <allissonabn@gmail.com>
2026-07-12 14:37:26 -0300

    feat(product-catalog): add security annotation to upload endpoint

--- src/main/java/br/dev/allissonnunes/algashop/product/catalog/presentation/UploadRequestController.java
+++ src/main/java/br/dev/allissonnunes/algashop/product/catalog/presentation/UploadRequestController.java
@@ -3,6 +3,7 @@ package br.dev.allissonnunes.algashop.product.catalog.presentation;
 import br.dev.allissonnunes.algashop.product.catalog.application.upload.UploadRequestApplicationService;
 import br.dev.allissonnunes.algashop.product.catalog.application.upload.UploadRequestInput;
 import br.dev.allissonnunes.algashop.product.catalog.application.upload.UploadResponseOutput;
+import br.dev.allissonnunes.algashop.product.catalog.infrastructure.security.SecurityAnnotations;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +19,7 @@ class UploadRequestController {
 
     private final UploadRequestApplicationService uploadRequestApplicationService;
 
+    @SecurityAnnotations.CanWriteProducts
     @PostMapping
     public ResponseEntity<UploadResponseOutput> requestUpload(@RequestBody final @Valid UploadRequestInput input) {
         return ResponseEntity.ok(uploadRequestApplicationService.requestPreSignedUrl(input));
